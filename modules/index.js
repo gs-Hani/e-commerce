e757e27497df136f6013d70bde6aa8e848fa061f
GS Hani <hani_alazawi@hotmail.com>
2022-10-13 11:49:33 +0300

    documentation visiualized

--- modules/index.js
+++ modules/index.js
@@ -1,11 +1,14 @@
 const express_sessionModule = require('./express-session');
 const passportModule        = require('./passport');
 const router                = require('../routes/index');
+const swagger               = require('./swagger');
 
 module.exports = async (app) => {
     
     const express_session = await express_sessionModule(app);
     const passport        = passportModule(express_session);
+
+    swagger(app);
 
     router(app, passport);
 };
